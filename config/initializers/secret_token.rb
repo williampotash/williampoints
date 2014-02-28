856e255942a55756ca5e788796ba45f7deec2efa
William Potash <william@williampotash.com>
2014-02-28 01:09:29 +0000

    Initialising the repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Williampoints::Application.config.secret_key_base = '32f4f492ba68c046b570f84460bcf85c2af0667c5be622c55b1da37a556e4d2907d86c602cf246abbf8b5a5652ea02d8c560f11c1971028fc427cabe527a01b5'
